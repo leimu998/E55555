35d607df271364af874b7d903862dc611136b14b
蕾姆 <41418251+leimu998@users.noreply.github.com>
2023-12-01 22:33:27 +0800

    更新 jju.css

---
+++ test./jju.css
@@ -0,0 +1,47 @@
+body {  
+    font-family: Arial, sans-serif;  
+    background-color: #f0f0f0;  
+    display: flex;  
+    justify-content: center;  
+    align-items: center;  
+    height: 100vh;  
+    margin: 0;  
+}  
+  
+.login-container {  
+    background-color: #fff;  
+    padding: 20px;  
+    border-radius: 10px;  
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);  
+}  
+  
+h2 {  
+    text-align: center;  
+    margin-bottom: 20px;  
+}  
+  
+.input-group {  
+    margin-bottom: 15px;  
+}  
+  
+label {  
+    display: block;  
+    margin-bottom: 5px;  
+}  
+  
+input[type="text"], input[type="password"] {  
+    width: 100%;  
+    padding: 10px;  
+    border: 1px solid #ccc;  
+    border-radius: 5px;  
+}  
+  
+button {  
+    width: 100%;  
+    padding: 10px;  
+    background-color: #4CAF50;  
+    color: white;  
+    border: none;  
+    border-radius: 5px;  
+    cursor: pointer;  
+}
