50230c847fa642a3ded4a916d2f699029dd5cabc
蕾姆 <41418251+leimu998@users.noreply.github.com>
2023-12-01 22:30:30 +0800

    更新 fcch.py

---
+++ test./fcch.py
@@ -0,0 +1,8 @@
+name = "小明"  
+age = 20  
+major = "计算机科学与技术"  
+hobbies = ["编程", "阅读", "旅游"]  
+  
+print(f"大家好，我叫{name}，今年{age}岁，是一名{major}专业的学生。")  
+print(f"我喜欢{hobbies[0]}、{hobbies[1]}和{hobbies[2]}。")  
+print(f"我的梦想是成为一名优秀的程序员，为人们提供更好的服务。")
